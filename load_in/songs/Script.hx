bc45268e1292f5867b1135879dcd5d93c93396ec
~ Care <126361810+usb-port-2@users.noreply.github.com>
2024-09-03 09:19:35 +0100

    Commit 1.75 (Charts and Metas)

--- load_in/songs/Script.hx
+++ load_in/songs/Script.hx
@@ -1,6 +1,5 @@
-function onDadHit(e)
+function onDadHit(e){
     e.characters = strumLines.members[e.noteType == "GF Sing" ? 2 : 0].characters;
-
-function onNoteHit(e)
-    if (!e.note.isSustainNote)
+    if (!e.note.isSustainNote && curCameraTarget != 1)
         curCameraTarget = strumLines.members.indexOf(e.note.strumLine);
+}
