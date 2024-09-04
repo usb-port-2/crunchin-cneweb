9094075743387d72a6dc8621cf93ebeb5b82cfd9
~ Care <126361810+usb-port-2@users.noreply.github.com>
2024-09-04 08:25:09 +0100

    testin

--- load_in/scripts/Song.hx
+++ load_in/scripts/Song.hx
@@ -3,3 +3,7 @@ function onDadHit(e){
     if (!e.note.isSustainNote && curCameraTarget != 1)
         curCameraTarget = strumLines.members.indexOf(e.note.strumLine);
 }
+
+function onNoteCreation(e)
+    if(!e.note.isSustainNote && e.noteType != "")
+        e.noteSprite = "game/notes/" + e.noteType;
