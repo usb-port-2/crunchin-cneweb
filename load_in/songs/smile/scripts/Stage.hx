3a357b3003707d9db0fd69e9c57d0de7823a9f55
~ Care <126361810+usb-port-2@users.noreply.github.com>
2024-09-04 09:15:16 +0100

    Saved memory

--- load_in/songs/smile/scripts/Stage.hx
+++ load_in/songs/smile/scripts/Stage.hx
@@ -1,3 +1,14 @@
 function postCreate() {
-    strumLines.members[1].characters[0].flipX = true;
+    strumLines.members[1].characters[0].flipX = healthBar.flipX = true;
+    strumLines.members[1].characters[0].swapLeftRightAnimations();
+
+    for(icon in [iconP1, iconP2])
+        icon.flipX = !icon.flipX;
+}
+
+function postUpdate() {
+	var center:Float = healthBar.x + healthBar.width * FlxMath.remapToRange(healthBar.percent, 100, 0, 1, 0);
+	iconP1.x = center - (iconP1.width - 26);
+	iconP2.x = center - 26;
+
 }
