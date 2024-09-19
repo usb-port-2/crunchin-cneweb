31677f73ebc341d56b4978e85af7d18d08ea1ac2
~ Care <126361810+usb-port-2@users.noreply.github.com>
2024-09-19 12:50:25 +0100

    reduced file size (please load quicker)

--- load_in/scripts/Song.hx
+++ load_in/scripts/Song.hx
@@ -1,4 +1,4 @@
 function onDadHit(e){e.characters = strumLines.members[e.noteType == "GF Sing" ? 2 : 0].characters; if(!e.note.isSustainNote && curCameraTarget != 1) curCameraTarget = strumLines.members.indexOf(e.note.strumLine);}
 function onNoteCreation(e) if(!e.note.isSustainNote && e.noteType != null) e.noteSprite = "game/notes/" + e.noteType;
-function onPlayerMiss(e) if(e.noteType != null){e.cancel(); e.animCancelled = true; strumLines.members[e.playerID].deleteNote(e.note);}
+function onPlayerMiss(e) if(e.noteType != null && e.noteType != "GF Sing"){e.cancel(); e.animCancelled = true; strumLines.members[e.playerID].deleteNote(e.note);}
 function onPlayerHit(e) if(e.noteType != null) health /= 2; // temp, until i do the mechanics
